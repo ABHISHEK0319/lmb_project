125bf6696a4a5da49365dd931e0803fe8fa35e6b
Abhishek Maurya <abhishekmaurya1219@gmail.com>
2022-12-08 14:46:07 +0530

    changes

--- lib/pages/Logedin.dart
+++ lib/pages/Logedin.dart
@@ -14,18 +14,14 @@ class _LogedinPageState extends State<LogedinPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: WebColors.bgcolor1,
       body: CustomScrollView(
         slivers: <Widget>[
-          //sliver app bar
           const SliverAppBar(
             backgroundColor: WebColors.bgcolor1,
             expandedHeight: 120,
-            //elevation: 20,
-            //collapsedHeight: 100,
             flexibleSpace: Header(),
             pinned: true,
-            //snap: true,
-            //floating: true,
           ),
 
           // sliver items
